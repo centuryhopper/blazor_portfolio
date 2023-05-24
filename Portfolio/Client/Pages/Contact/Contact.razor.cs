b4b2a94123492c2f8a660ff23f0cf4dba3a81fbb
Leo Zhang <leotheasianlion@gmail.com>
2023-05-24 09:38:34 -0400

    blazor modal now working

--- Portfolio/Client/Pages/Contact/Contact.razor.cs
+++ Portfolio/Client/Pages/Contact/Contact.razor.cs
@@ -1,5 +1,7 @@
 using System.Net.Http.Json;
+using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
+using Portfolio.Client.Shared;
 using Portfolio.Shared;
 
 namespace Portfolio.Client.Pages;
@@ -9,18 +11,25 @@ public class ContactBase : ComponentBase
     [Inject]
     private IHttpClientFactory httpClientFactory { get; set; }
 
+    [Inject]
+    private IModalService Modal {get; set;}
+
     protected ContactMeModel Model = new();
 
     protected async Task HandlePostContact()
     {
-        System.Console.WriteLine($"posting model: {Model}");
-        var client = httpClientFactory.CreateClient("API");
-        var response = await client.PostAsJsonAsync("/api/Contacts/post-contact", Model);
-        if (response.IsSuccessStatusCode)
-        {
-            Console.WriteLine(response.Content.ReadAsStringAsync());
-            Model = new();
-        }
+        // System.Console.WriteLine($"posting model: {Model}");
+        // var client = httpClientFactory.CreateClient("API");
+        // var response = await client.PostAsJsonAsync("/api/Contacts/post-contact", Model);
+        // if (response.IsSuccessStatusCode)
+        // {
+        //     Console.WriteLine(response.Content.ReadAsStringAsync());
+        //     Model = new();
+        //     Modal.Show<PopupDialog>("notice");
+        // }
+
+        Modal.Show<PopupDialog>("notice");
+
     }
 
 
